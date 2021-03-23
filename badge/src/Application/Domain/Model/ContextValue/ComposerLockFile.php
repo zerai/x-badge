711765dcb1d3a3a9ba1d854037321b663e536e53
zerai <teclaizerai@gmail.com>
2021-03-23 10:28:49 +0100

    Aggiunto VO ComposerLockFile.

---
+++ badge/src/Application/Domain/Model/ContextValue/ComposerLockFile.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+namespace Badge\Application\Domain\Model\ContextValue;
+
+use Badge\Application\Domain\Model\ContextValue\Common\CommittedFile;
+
+final class ComposerLockFile extends CommittedFile
+{
+    public static function createAsCommitted(): self
+    {
+        return new self('committed');
+    }
+
+    public static function createAsUncommitted(): self
+    {
+        return new self('uncommitted');
+    }
+
+    public static function createAsUndetected(): self
+    {
+        return new self('uncommitted');
+    }
+}
---
+++ badge/tests/Unit/ContextValue/ComposerLockFileTest.php
@@ -0,0 +1,63 @@
+<?php declare(strict_types=1);
+
+namespace Badge\Tests\Unit\ContextValue;
+
+use Badge\Application\Domain\Model\ContextualizableValue;
+use Badge\Application\Domain\Model\ContextValue\Common\CommittedFile;
+use Badge\Application\Domain\Model\ContextValue\ComposerLockFile;
+use PHPUnit\Framework\TestCase;
+
+final class ComposerLockFileTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function canBeCreated(): void
+    {
+        $sut = new ComposerLockFile('committed');
+        self::assertInstanceOf(ContextualizableValue::class, $sut);
+        self::assertInstanceOf(CommittedFile::class, $sut);
+        self::assertInstanceOf(ComposerLockFile::class, $sut);
+
+        $sut = ComposerLockFile::createAsCommitted();
+        self::assertInstanceOf(ContextualizableValue::class, $sut);
+        self::assertInstanceOf(CommittedFile::class, $sut);
+        self::assertInstanceOf(ComposerLockFile::class, $sut);
+    }
+
+    /**
+     * @test
+     */
+    public function canBeCreatedAsCommittedWithFactoryMethod(): void
+    {
+        $sut = ComposerLockFile::createAsCommitted();
+
+        self::assertInstanceOf(ContextualizableValue::class, $sut);
+        self::assertInstanceOf(CommittedFile::class, $sut);
+        self::assertInstanceOf(ComposerLockFile::class, $sut);
+    }
+
+    /**
+     * @test
+     */
+    public function canBeCreatedAsUncommittedWithFactoryMethod(): void
+    {
+        $sut = ComposerLockFile::createAsUncommitted();
+
+        self::assertInstanceOf(ContextualizableValue::class, $sut);
+        self::assertInstanceOf(CommittedFile::class, $sut);
+        self::assertInstanceOf(ComposerLockFile::class, $sut);
+    }
+
+    /**
+     * @test
+     */
+    public function canBeCreatedAsUndetectedWithFactoryMethod(): void
+    {
+        $sut = ComposerLockFile::createAsUndetected();
+
+        self::assertInstanceOf(ContextualizableValue::class, $sut);
+        self::assertInstanceOf(CommittedFile::class, $sut);
+        self::assertInstanceOf(ComposerLockFile::class, $sut);
+    }
+}

5b26addd9fa96ccc6a4a021a9599b09e7e38904c
zerai <teclaizerai@gmail.com>
2021-03-24 12:44:19 +0100

    Aggiunto servizio DefaultBranchDetector.

---
+++ badge/src/Adapter/Out/DefaultBranchDetector.php
@@ -0,0 +1,47 @@
+<?php declare(strict_types=1);
+
+namespace Badge\Adapter\Out;
+
+use Badge\Application\Domain\Model\RepositoryDetail;
+use Badge\Application\Domain\Model\Service\DefaultBranchDetector\DetectableBranch;
+use Bitbucket\Client as BitbucketClient;
+use Github\Api\Repo;
+use Github\Client as GithubClient;
+
+final class DefaultBranchDetector implements DetectableBranch
+{
+    private GithubClient $githubClient;
+
+    private BitbucketClient $bitbucketClient;
+
+    public function __construct(GithubClient $githubClient, BitbucketClient $bitbucketClient)
+    {
+        $this->githubClient = $githubClient;
+        $this->bitbucketClient = $bitbucketClient;
+    }
+
+    public function getDefaultBranch(RepositoryDetail $repositoryDetail): string
+    {
+        $result = '';
+
+        if ($repositoryDetail->isGitHub()) {
+            $repoGitHubData = $this->githubClient
+                ->repo()
+                ->show($repositoryDetail->repositoryOwner(), $repositoryDetail->repositoryName());
+
+            $result = (string) $repoGitHubData['default_branch'];
+        }
+
+        if ($repositoryDetail->isBitbucket()) {
+            /** @var array<mixed> $repoBitbucketData */
+            $repoBitbucketData = $this->bitbucketClient
+                ->repositories()
+                ->workspaces($repositoryDetail->repositoryOwner())
+                ->show($repositoryDetail->repositoryName());
+
+            $result = (string) $repoBitbucketData['mainbranch']['name'];
+        }
+
+        return $result;
+    }
+}

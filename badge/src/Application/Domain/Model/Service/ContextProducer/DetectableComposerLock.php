186e469638ceade989d1576226466a8b6bd9669b
zerai <teclaizerai@gmail.com>
2021-03-24 13:27:19 +0100

    Aggiunta interfaccia DetectableComposerLock.

---
+++ badge/src/Application/Domain/Model/Service/ContextProducer/DetectableComposerLock.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types=1);
+
+namespace Badge\Application\Domain\Service\ContextProducer;
+
+interface DetectableComposerLock
+{
+    /**
+     * @param string $packageName full package name "[vendor]/[package]"
+     */
+    public function detectComposerLock(string $packageName): int;
+}

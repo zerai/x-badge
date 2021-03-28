85e1d1971c2fe0d895a90ae5c1ae9ed4ea9d74a9
zerai <teclaizerai@gmail.com>
2021-03-28 13:35:21 +0200

    Cambio in Interface ContextProducer, RenderableValue come valore di ritorno.

--- badge/src/Application/Domain/Model/Service/ContextProducer/ContextProducer.php
+++ badge/src/Application/Domain/Model/Service/ContextProducer/ContextProducer.php
@@ -3,8 +3,10 @@
 namespace Badge\Application\Domain\Model\Service\ContextProducer;
 
 use Badge\Application\Domain\Model\BadgeContext\BadgeContext;
+use Badge\Application\Domain\Model\RenderableValue;
 
 interface ContextProducer
 {
-    public function contextFor(string $packageName): BadgeContext;
+    //public function contextFor(string $packageName): BadgeContext;
+    public function contextFor(string $packageName): RenderableValue;
 }

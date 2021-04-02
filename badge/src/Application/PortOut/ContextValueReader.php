5a7d0bf6429f584e544353f16d4817fe6ee1da85
zerai <teclaizerai@gmail.com>
2021-04-02 04:54:08 +0200

    Aggiunta interfaccia ContextValueReader (PortOut).

---
+++ badge/src/Application/PortOut/ContextValueReader.php
@@ -0,0 +1,9 @@
+<?php declare(strict_types=1);
+
+namespace Badge\Application\PortOut;
+
+use Badge\Application\Domain\Model\Service\ContextProducer\SuggestersReader;
+
+interface ContextValueReader extends SuggestersReader
+{
+}

ba3805ab25e48bf5b8f223d301b35440c38ef9d2
zerai <teclaizerai@gmail.com>
2021-04-11 14:50:05 +0200

    Aggiunto ReleasedVersions VO, tests e fixtures.

---
+++ badge/src/Infrastructure/ReleasedVersions.php
@@ -0,0 +1,229 @@
+<?php declare(strict_types=1);
+
+namespace Badge\Infrastructure;
+
+use Packagist\Api\Result\Package\Version;
+
+final class ReleasedVersions
+{
+    private static string $modifierRegex = '[._-]?(?:(stable|beta|b|RC|alpha|a|patch|pl|p)(?:[.-]?(\d+))?)?([.-]?dev)?';
+
+    private ?string $license = null;
+
+    private ?string $latestStableVersion = null;
+
+    private ?string $latestUnstableVersion = null;
+
+    private ?string $latestStableVersionNormalized = null;
+
+    private ?string $latestUnstableVersionNormalized = null;
+
+    /**
+     * @var array<Version[]>
+     */
+    private $versions = [];
+
+    /**
+     * @param array<Version[]> $versions
+     */
+    public function __construct(array $versions)
+    {
+        $this->versions = $versions;
+        $this->calculateLatestVersions();
+    }
+
+    /**
+     * @param array<Version[]> $versions
+     */
+    public static function fromApiData(array $versions): self
+    {
+        return new self($versions);
+    }
+
+    public function stableVersion(): ?string
+    {
+        return $this->latestStableVersion;
+    }
+
+    public function unstableVersion(): ?string
+    {
+        return $this->latestUnstableVersion;
+    }
+
+    public function license(): ?string
+    {
+        return $this->license;
+    }
+
+    /**
+     * Returns the stability of a version.
+     *
+     * This function is part of Composer.
+     *
+     * (c) Nils Adermann <naderman@naderman.de>
+     * Jordi Boggiano <j.boggiano@seld.be>
+     */
+    public static function parseStability(string $version): string
+    {
+        $version = \preg_replace('{#.+$}i', '', $version) ?? '';
+
+        if (\substr($version, 0, 4) === 'dev-' || \substr($version, -4) === '-dev') {
+            return 'dev';
+        }
+
+        \preg_match('{' . self::$modifierRegex . '$}i', \strtolower($version), $match);
+        if (! empty($match[3])) {
+            return 'dev';
+        }
+
+        if (! empty($match[1])) {
+            if ($match[1] === 'beta' || $match[1] === 'b') {
+                return 'beta';
+            }
+            if ($match[1] === 'alpha' || $match[1] === 'a') {
+                return 'alpha';
+            }
+            if ($match[1] === 'rc') {
+                return 'RC';
+            }
+        }
+
+        return 'stable';
+    }
+
+    private function getLatestStableVersion(): ?string
+    {
+        return $this->latestStableVersion;
+    }
+
+    private function getLatestUnstableVersion(): ?string
+    {
+        return $this->latestUnstableVersion;
+    }
+
+    private function getLatestStableVersionNormalized(): ?string
+    {
+        return $this->latestStableVersionNormalized;
+    }
+
+    private function getLatestUnstableVersionNormalized(): ?string
+    {
+        return $this->latestUnstableVersionNormalized;
+    }
+
+    private function getLicense(): ?string
+    {
+        return $this->license;
+    }
+
+    private function calculateLatestVersions(): self
+    {
+        $versions = $this->versions;
+
+        \usort($versions, [$this, 'comparator']);
+
+        /** @var Version $version */
+        foreach ($versions as $name => $version) {
+            $currentVersionName = $version->getVersion();
+            $versionNormalized = $version->getVersionNormalized();
+
+            $aliases = $this->getBranchAliases($version);
+            if ($aliases !== null && \array_key_exists($currentVersionName, $aliases)) {
+                $currentVersionName = $aliases[$currentVersionName];
+            }
+
+            $functionName = 'Unstable';
+            if (self::parseStability($currentVersionName) === 'stable') {
+                $functionName = 'Stable';
+            }
+
+            /**
+             * START CODICE AGGIUNTO:
+             *
+             * Fix exception all'inizio del loop, la funzione \version_compare()
+             * vuole 2 argomenti di tipo Version,
+             * ma al primo ciclo $this->{'getLatest' . $functionName . 'VersionNormalized'}()
+             * e sempre null.
+             */
+            if (null === $this->{'getLatest' . $functionName . 'VersionNormalized'}()) {
+                $this->{'setLatest' . $functionName . 'Version'}($currentVersionName);
+                $this->{'setLatest' . $functionName . 'VersionNormalized'}($versionNormalized);
+            }
+            /**
+             * END CODICE AGGIUNTO:
+             */
+
+            if (\version_compare($versionNormalized, $this->{'getLatest' . $functionName . 'VersionNormalized'}()) > 0) {
+                $this->{'setLatest' . $functionName . 'Version'}($currentVersionName);
+                $this->{'setLatest' . $functionName . 'VersionNormalized'}($versionNormalized);
+                /** @var string|string[] $license */
+                $license = $version->getLicense();
+
+                $this->setLicense($this->normalizeLicense($license));
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * Get all the branch aliases.
+     *
+     * @return array<string, string>|null
+     */
+    private function getBranchAliases(Version $version): ?array
+    {
+        $extra = $version->getExtra();
+        if ($extra !== null && isset($extra['branch-alias']) && \is_array($extra['branch-alias'])) {
+            return $extra['branch-alias'];
+        }
+
+        return null;
+    }
+
+    private function setLicense(string $license): void
+    {
+        $this->license = $license;
+    }
+
+    /**
+     * @param string|string[] $licenseData
+     */
+    private function normalizeLicense($licenseData): string
+    {
+        if (! \is_array($licenseData)) {
+            return $licenseData;
+        }
+
+        if (\count($licenseData) > 0) {
+            return \implode(',', $licenseData);
+        }
+
+        return '';
+    }
+
+    private function comparator(Version $version1, Version $version2): int
+    {
+        return $version1->getTime() <=> $version2->getTime();
+    }
+
+    private function setLatestStableVersion(string $version): void
+    {
+        $this->latestStableVersion = $version;
+    }
+
+    private function setLatestUnstableVersion(string $version): void
+    {
+        $this->latestUnstableVersion = $version;
+    }
+
+    private function setLatestStableVersionNormalized(string $latestStableVersionNormalized): void
+    {
+        $this->latestStableVersionNormalized = $latestStableVersionNormalized;
+    }
+
+    private function setLatestUnstableVersionNormalized(string $latestUnstableVersionNormalized): void
+    {
+        $this->latestUnstableVersionNormalized = $latestUnstableVersionNormalized;
+    }
+}
---
+++ badge/tests/Unit/Infrastructure/ReleasedVersions/ReleasedVersionsTest.php
@@ -0,0 +1,110 @@
+<?php declare(strict_types=1);
+
+namespace Badge\Tests\Unit\Infrastructure;
+
+use Badge\Infrastructure\ReleasedVersions;
+use Generator;
+use Packagist\Api\Result\Package\Version;
+use PHPUnit\Framework\TestCase;
+
+final class ReleasedVersionsTest extends TestCase
+{
+    /**
+     * @test
+     * @dataProvider lastStableVersionDataProvider
+     * @param array<Version[]> $versions
+     */
+    public function canDetectLastStableVersion(array $versions, string $expectedVersion): void
+    {
+        $sut = ReleasedVersions::fromApiData($versions);
+
+        self::assertEquals($expectedVersion, $sut->stableVersion());
+    }
+
+    /**
+     * @psalm-return Generator<string, array{0: array<array-key, array<array-key, Version>>, 1: string}, mixed, void>
+     */
+    public function lastStableVersionDataProvider(): Generator
+    {
+        yield 'badges/poser versions' => [$this->loadFixturesForPackage('badges/poser'), 'v2.0.4'];
+        yield 'doctrine/collections versions' => [$this->loadFixturesForPackage('doctrine/collections'), '1.6.7'];
+        yield 'symfony/contracts versions' => [$this->loadFixturesForPackage('symfony/contracts'), 'v2.3.1'];
+    }
+
+    /**
+     * @test
+     * @dataProvider lastUnstableVersionDataProvider
+     * @param array<Version[]> $versions
+     */
+    public function canDetectLastUnstableVersion(array $versions, string $expectedVersion): void
+    {
+        $sut = ReleasedVersions::fromApiData($versions);
+
+        self::assertEquals($expectedVersion, $sut->unstableVersion());
+    }
+
+    /**
+     * @psalm-return Generator<string, array{0: array<array-key, array<array-key, Version>>, 1: string}, mixed, void>
+     */
+    public function lastUnstableVersionDataProvider(): Generator
+    {
+        yield 'badges/poser versions' => [$this->loadFixturesForPackage('badges/poser'), '1.4.x-dev'];
+        yield 'doctrine/collections versions' => [$this->loadFixturesForPackage('doctrine/collections'), '2.0.x-dev'];
+        yield 'symfony/contracts versions' => [$this->loadFixturesForPackage('symfony/contracts'), 'v1.0.0-RC1'];
+        yield 'ramsey/uuid versions' => [$this->loadFixturesForPackage('ramsey/uuid'), '4.0.0-beta2'];
+    }
+
+    /**
+     * @test
+     * @dataProvider licenseDataProvider
+     * @param array<Version[]> $versions
+     */
+    public function canDetectLicense(array $versions, string $expectedLicense): void
+    {
+        $sut = ReleasedVersions::fromApiData($versions);
+
+        self::assertEquals($expectedLicense, $sut->license());
+    }
+
+    /**
+     * @psalm-return Generator<string, array{0: array<array-key, array<array-key, Version>>, 1: string}, mixed, void>
+     */
+    public function licenseDataProvider(): Generator
+    {
+        yield 'badges/poser versions' => [$this->loadFixturesForPackage('badges/poser'), 'MIT'];
+        yield 'doctrine/collections versions' => [$this->loadFixturesForPackage('doctrine/collections'), 'MIT'];
+        yield 'symfony/contracts versions' => [$this->loadFixturesForPackage('symfony/contracts'), 'MIT'];
+    }
+
+    /**
+     * @return array<array<Version>>
+     */
+    private function loadFixturesForPackage(string $packageName): array
+    {
+        $string_data = '';
+        switch ($packageName) {
+            case 'badges/poser':
+                $string_data = (string) \file_get_contents(
+                    \dirname(__DIR__) . '/ReleasedVersions/serialized-versions-for-package-badges-poser.txt'
+                ); break;
+            case 'doctrine/collections':
+                $string_data = (string) \file_get_contents(
+                    \dirname(__DIR__) . '/ReleasedVersions/serialized-versions-for-package-doctrine-collections.txt'
+                );
+                break;
+            case 'symfony/contracts':
+                $string_data = (string) \file_get_contents(
+                    \dirname(__DIR__) . '/ReleasedVersions/serialized-versions-for-package-symfony-contracts.txt'
+                );
+                break;
+            case 'ramsey/uuid':
+                $string_data = (string) \file_get_contents(
+                    \dirname(__DIR__) . '/ReleasedVersions/serialized-versions-for-package-ramsey-uuid.txt'
+                );
+                break;
+
+        }
+        /** @psalm-suppress all */
+        return \unserialize($string_data);
+    }
+}

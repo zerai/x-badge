24ee9e4f18a86b6967e40ee0e4897f8f50ef1d70
zerai <teclaizerai@gmail.com>
2021-04-14 16:17:42 +0200

    Aggiunti dataProvider in fail test PoserImageFactoryTest.

--- badge/tests/Unit/Infrastructure/PoserImageFactoryTest.php
+++ badge/tests/Unit/Infrastructure/PoserImageFactoryTest.php
@@ -6,8 +6,11 @@ use Badge\Application\BadgeImage;
 use Badge\Application\Domain\Model\BadgeContext;
 use Badge\Application\Domain\Model\ContextValue\ComposerLockFile;
 use Badge\Application\Domain\Model\ContextValue\DailyDownloads;
+use Badge\Application\Domain\Model\ContextValue\Dependents;
 use Badge\Application\Domain\Model\ContextValue\GitAttributesFile;
 use Badge\Application\Domain\Model\ContextValue\MonthlyDownloads;
+use Badge\Application\Domain\Model\ContextValue\StableVersion;
+use Badge\Application\Domain\Model\ContextValue\Suggesters;
 use Badge\Application\Domain\Model\ContextValue\TotalDownloads;
 use Badge\Application\Image;
 use Badge\Infrastructure\PoserImageFactory;
@@ -50,6 +53,9 @@ final class PoserImageFactoryTest extends TestCase
      * @test
      * @dataProvider renderableCommittedFileDataProvider
      * @dataProvider renderableDownloadDataProvider
+     * @dataProvider renderableDependentsDataProvider
+     * @dataProvider renderableSuggestersDataProvider
+     * @dataProvider renderableVersionssDataProvider
      * @param string $expectedFileName
      */
     public function canCreateImageFromContext(BadgeContext $badgeContext, $expectedFileName): void
@@ -80,13 +86,46 @@ final class PoserImageFactoryTest extends TestCase
     public function renderableDownloadDataProvider(): Generator
     {
         yield 'a total download' => [new TotalDownloads(10), 'downloads-10-007ec6.svg'];
-        // TODO rimuovere il . nel file name
-        yield 'a total download whit normalized counter ' => [new TotalDownloads(100588), 'downloads-100.59-k-007ec6.svg'];
+        // TODO rimuovere il . nel file name | rimuovere gli spazi
+        yield 'a total download with normalized counter ' => [new TotalDownloads(100588), 'error----downloads-100.59-k-007ec6.svg'];
         yield 'a monthly download' => [new MonthlyDownloads(10), 'downloads-10-this-month-007ec6.svg'];
-        // TODO rimuovere il . nel file name
-        yield 'a monthly download with normalized counter' => [new MonthlyDownloads(1032555), 'downloads-1.03-M-this-month-007ec6.svg'];
+        // TODO rimuovere il . nel file name | rimuovere gli spazi
+        yield 'a monthly download with normalized counter' => [new MonthlyDownloads(1032555), 'error----downloads-1.03-M-this-month-007ec6.svg'];
         yield 'a daily download' => [new DailyDownloads(10), 'downloads-10-today-007ec6.svg'];
+        // TODO rimuovere il . nel file name | rimuovere gli spazi
+        yield 'a daily download with normalized counter' => [new DailyDownloads(320860055), 'error----downloads-320.86-M-today-007ec6.svg'];
+    }
+
+    /**
+     * @psalm-return Generator<string,  array{0: BadgeContext, 1: string}>
+     */
+    public function renderableDependentsDataProvider(): Generator
+    {
+        yield 'a dependents' => [new Dependents(10), 'dependents-10-007ec6.svg'];
+        // TODO rimuovere il . nel file name | rimuovere gli spazi
+        yield 'a dependents with normalized counter ' => [new Dependents(100588), 'error----dependents-100.59-k-007ec6.svg'];
+    }
+
+    /**
+     * @psalm-return Generator<string,  array{0: BadgeContext, 1: string}>
+     */
+    public function renderableSuggestersDataProvider(): Generator
+    {
+        yield 'a suggesters' => [new Suggesters(10), 'suggesters-10-007ec6.svg'];
+        // TODO rimuovere il . nel file name | rimuovere gli spazi
+        yield 'a suggesters with normalized counter ' => [new Suggesters(100588), 'error----suggesters-100.59-k-007ec6.svg'];
+    }
+
+    /**
+     * @psalm-return Generator<string,  array{0: BadgeContext, 1: string}>
+     */
+    public function renderableVersionssDataProvider(): Generator
+    {
         // TODO rimuovere il . nel file name
-        yield 'a daily download with normalized counter' => [new DailyDownloads(320860055), 'downloads-320.86-M-today-007ec6.svg'];
+        yield 'a stable version' => [StableVersion::fromString('1.0.5'), 'error----stable-1.0.5-28a3df.svg'];
+        // TODO | rimuovere gli spazi
+        yield 'a stable version with no-release' => [StableVersion::withNoRelease(), 'error----stable-No Release-28a3df.svg'];
+        // TODO rimuovere il . nel file name
+        yield 'an unstable version' => [StableVersion::fromString('1.0.5-dev'), 'error----stable-1.0.5-dev-28a3df.svg'];
     }
 }

428b34dc90dc26fe0f7b210c3ceb067e27380a42
zerai <teclaizerai@gmail.com>
2021-04-16 17:07:10 +0200

    Aggiunto test per VersionStability.

---
+++ badge/tests/Unit/Infrastructure/ReleasedVersions/VersionStabilityTest.php
@@ -0,0 +1,13 @@
+<?php declare(strict_types=1);
+
+namespace Badge\Tests\Unit\Infrastructure\ReleasedVersions;
+
+use PHPUnit\Framework\TestCase;
+
+final class VersionStabilityTest extends TestCase
+{
+    public function canBeCreated(): void
+    {
+        self::markTestIncomplete();
+    }
+}

f8fa839839d2dfe51f5c52435462865c57e8b04a
zerai <teclaizerai@gmail.com>
2021-04-16 18:25:29 +0200

    Aggiunto VO VersionStability.

---
+++ badge/src/Infrastructure/VersionStability.php
@@ -0,0 +1,59 @@
+<?php declare(strict_types=1);
+
+namespace Badge\Infrastructure;
+
+use InvalidArgumentException;
+
+final class VersionStability
+{
+    private const MODIFIER_REGEX = '[._-]?(?:(stable|beta|b|RC|alpha|a|patch|pl|p)(?:[.-]?(\d+))?)?([.-]?dev)?';
+
+    private string $version;
+
+    private function __construct(string $version)
+    {
+        $this->version = $this->validate($version);
+    }
+
+    public static function fromString(string $version): self
+    {
+        return new self($version);
+    }
+
+    public function detect(): string
+    {
+        $version = \preg_replace('{#.+$}i', '', $this->version) ?? '';
+
+        if (\substr($version, 0, 4) === 'dev-' || \substr($version, -4) === '-dev') {
+            return 'dev';
+        }
+
+        \preg_match('{' . self::MODIFIER_REGEX . '$}i', \strtolower($version), $match);
+        if (! empty($match[3])) {
+            return 'dev';
+        }
+
+        if (! empty($match[1])) {
+            if ($match[1] === 'beta' || $match[1] === 'b') {
+                return 'beta';
+            }
+            if ($match[1] === 'alpha' || $match[1] === 'a') {
+                return 'alpha';
+            }
+            if ($match[1] === 'rc') {
+                return 'RC';
+            }
+        }
+
+        return 'stable';
+    }
+
+    private function validate(string $input): string
+    {
+        if ($input === '') {
+            throw new InvalidArgumentException(\sprintf('Error: %s s not a valid version', $input));
+        }
+
+        return $input;
+    }
+}
--- badge/tests/Unit/Infrastructure/ReleasedVersions/VersionStabilityTest.php
+++ badge/tests/Unit/Infrastructure/ReleasedVersions/VersionStabilityTest.php
@@ -2,12 +2,30 @@
 
 namespace Badge\Tests\Unit\Infrastructure\ReleasedVersions;
 
+use Badge\Infrastructure\VersionStability;
+use InvalidArgumentException;
 use PHPUnit\Framework\TestCase;
 
+/** @covers \Badge\Infrastructure\VersionStability */
 final class VersionStabilityTest extends TestCase
 {
-    public function canBeCreated(): void
+    /**
+     * @test
+     */
+    public function emptyVersionInConstructorShouldThrowException(): void
     {
-        self::markTestIncomplete();
+        self::expectException(InvalidArgumentException::class);
+
+        VersionStability::fromString('');
+    }
+
+    /**
+     * @test
+     */
+    public function shouldDetectTheStabilty(string $version = '1.5.0', string $expectedStability = 'stable'): void
+    {
+        $stability = VersionStability::fromString($version)->detect();
+
+        self::assertEquals($expectedStability, $stability);
     }
 }

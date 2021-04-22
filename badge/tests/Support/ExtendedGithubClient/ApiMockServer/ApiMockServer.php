407067e25f20503fe32048667f9fd4573f3942da
zerai <teclaizerai@gmail.com>
2021-04-22 09:14:57 +0200

    Rimosso Infrastructure/FakeGithubServer.

---
+++ badge/tests/Support/ExtendedGithubClient/ApiMockServer/ApiMockServer.php
@@ -0,0 +1,124 @@
+<?php declare(strict_types=1);
+
+namespace Badge\Tests\Support\ExtendedGithubClient\ApiMockServer;
+
+use Badge\Infrastructure\Env;
+use RuntimeException;
+
+class ApiMockServer
+{
+    private const EXPECTATION_ENDPOINT = '/mockserver/expectation';
+
+    private const RESET_ENDPOINT = '/mockserver/reset';
+
+    public static function loadFixture(string $endpoint, string $jsonResponse): void
+    {
+        $expectetionTemplate = '
+            {
+                "httpRequest" : {
+                    "method" : "GET",
+                    "path" : "%s"
+                },
+                "httpResponse" : {
+                    "statusCode": 200,
+                    "body" : %s
+                }
+            }'
+        ;
+
+        $expectationDataRequest = \trim(\sprintf($expectetionTemplate, $endpoint, $jsonResponse));
+
+        $ch = \curl_init();
+
+        \curl_setopt($ch, CURLOPT_URL, self::expectationEndpoint());
+        \curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        \curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
+        \curl_setopt($ch, CURLOPT_POSTFIELDS, $expectationDataRequest);
+
+        $headers = [];
+        $headers[] = 'Content-Type: application/x-www-form-urlencoded';
+        \curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+        try {
+            $result = \curl_exec($ch);
+            if (\curl_errno($ch)) {
+                self::reset();
+
+                throw new RuntimeException('ApiMockServer error: ' . \curl_error($ch));
+            }
+            \curl_close($ch);
+        } catch (\Throwable $th) {
+            \curl_close($ch);
+
+            throw $th;
+        }
+    }
+
+    public static function loadGithubFixtureForPackage(string $endpoint, string $jsonResponse): void
+    {
+        $expectetionTemplate = '
+            {
+                "httpRequest" : {
+                    "method" : "GET",
+                    "path" : "%s"
+                },
+                "httpResponse" : {
+                    "statusCode": 200,
+                    "body" : %s
+                }
+            }'
+        ;
+
+        $expectationDataRequest = \trim(\sprintf($expectetionTemplate, $endpoint, $jsonResponse));
+
+        $ch = \curl_init();
+
+        \curl_setopt($ch, CURLOPT_URL, self::expectationEndpoint());
+        \curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        \curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
+        \curl_setopt($ch, CURLOPT_POSTFIELDS, $expectationDataRequest);
+
+        $headers = [];
+        $headers[] = 'Content-Type: application/x-www-form-urlencoded';
+        \curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+        try {
+            $result = \curl_exec($ch);
+            if (\curl_errno($ch)) {
+                self::reset();
+
+                throw new RuntimeException('ApiMockServer error: ' . \curl_error($ch));
+            }
+            \curl_close($ch);
+        } catch (\Throwable $th) {
+            \curl_close($ch);
+
+            throw $th;
+        }
+    }
+
+    public static function reset(): void
+    {
+        $ch = \curl_init();
+
+        \curl_setopt($ch, CURLOPT_URL, self::resetEndpoint());
+        \curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        \curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'PUT');
+
+        $result = \curl_exec($ch);
+        if (\curl_errno($ch)) {
+            echo 'Error:' . \curl_error($ch);
+        }
+        \curl_close($ch);
+    }
+
+    private static function expectationEndpoint(): string
+    {
+        return Env::get('API_MOCK_SERVER') . SELF::EXPECTATION_ENDPOINT;
+    }
+
+    private static function resetEndpoint(): string
+    {
+        return Env::get('API_MOCK_SERVER') . SELF::RESET_ENDPOINT;
+    }
+}
--- badge/tests/Support/ExtendedGithubClient/ExtendedForTestGithubClientTest.php
+++ badge/tests/Support/ExtendedGithubClient/ExtendedForTestGithubClientTest.php
@@ -3,15 +3,27 @@
 namespace Badge\Tests\Support\ExtendedGithubClient;
 
 use Badge\Infrastructure\Env;
+use Badge\Tests\Support\ExtendedGithubClient\ApiMockServer\ApiMockServer;
 use Github\Exception\RuntimeException;
 use InvalidArgumentException;
 use PHPUnit\Framework\TestCase;
 
 /**
  * @group io-network
+ * @covers \Badge\Tests\Support\ExtendedGithubClient\ExtendedForTestGithubClient
  */
 final class ExtendedForTestGithubClientTest extends TestCase
 {
+    public static function setUpBeforeClass(): void
+    {
+        self::loadFixtureToApiMockServer();
+    }
+
+    public static function tearDownAfterClass(): void
+    {
+        ApiMockServer::reset();
+    }
+
     /**
      * @test
      */
@@ -29,9 +41,8 @@ final class ExtendedForTestGithubClientTest extends TestCase
     public function unknownRepositoryReturnErrorNotFound(): void
     {
         self::expectException(RuntimeException::class);
-        self::expectExceptionMessage('Page not found');
 
-        $extendedClient = new ExtendedForTestGithubClient(Env::get('FAKE_GITHUB_API_URI'));
+        $extendedClient = new ExtendedForTestGithubClient(Env::get('API_MOCK_SERVER'));
 
         $extendedClient->repos()->show('foo', 'bar');
     }
@@ -41,10 +52,28 @@ final class ExtendedForTestGithubClientTest extends TestCase
      */
     public function canReturnAStubedResponseForKnownRepository(): void
     {
-        $extendedClient = new ExtendedForTestGithubClient(Env::get('FAKE_GITHUB_API_URI'));
+        $extendedClient = new ExtendedForTestGithubClient(Env::get('API_MOCK_SERVER'));
 
         $response = $extendedClient->repos()->show('sebastianbergmann', 'phpunit');
 
         self::assertNotEmpty($response);
+    }
+
+    private static function loadFixtureToApiMockServer(): void
+    {
+        ApiMockServer::loadFixture(
+            '/repos/sebastianbergmann/phpunit',
+            self::getFixtureContent(__DIR__ . '/repository-sebastianbergmann-phpunit.json')
+        );
+    }
+
+    private static function getFixtureContent(string $fixtureFile): string
+    {
+        if (! \file_exists($fixtureFile)) {
+            throw new RuntimeException(\sprintf('Fixture file: %s not found.', $fixtureFile));
+        }
+
+        /** @phpstan-ignore-next-line */
+        return \file_get_contents($fixtureFile);
     }
 }

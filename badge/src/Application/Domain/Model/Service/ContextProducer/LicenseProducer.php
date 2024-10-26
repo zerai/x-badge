7b7fe07329d248ce45675a473816ba35653f5ba2
zerai <teclaizerai@gmail.com>
2024-10-26 09:08:24 +0200

    ForReadingLicense: changed namespace.

--- badge/src/Application/Domain/Model/Service/ContextProducer/LicenseProducer.php
+++ badge/src/Application/Domain/Model/Service/ContextProducer/LicenseProducer.php
@@ -4,6 +4,7 @@ namespace Badge\Application\Domain\Model\Service\ContextProducer;
 
 use Badge\Application\Domain\Model\BadgeContext;
 use Badge\Application\Domain\Model\ContextValue\License;
+use Badge\Application\Port\Driven\ForReadingLicense;
 
 class LicenseProducer implements ContextProducer
 {
--- badge/src/Application/PortOut/ContextValueReader.php
+++ badge/src/Application/PortOut/ContextValueReader.php
@@ -2,7 +2,6 @@
 
 namespace Badge\Application\PortOut;
 
-use Badge\Application\Domain\Model\Service\ContextProducer\ForReadingLicense;
 use Badge\Application\Domain\Model\Service\ContextProducer\ForReadingMonthlyDownloads;
 use Badge\Application\Domain\Model\Service\ContextProducer\ForReadingStableVersion;
 use Badge\Application\Domain\Model\Service\ContextProducer\ForReadingSuggesters;
@@ -10,6 +9,7 @@ use Badge\Application\Domain\Model\Service\ContextProducer\ForReadingTotalDownlo
 use Badge\Application\Domain\Model\Service\ContextProducer\ForReadingUnstableVersion;
 use Badge\Application\Port\Driven\ForReadingDailyDownloads;
 use Badge\Application\Port\Driven\ForReadingDependents;
+use Badge\Application\Port\Driven\ForReadingLicense;
 
 interface ContextValueReader extends
     ForReadingSuggesters,
